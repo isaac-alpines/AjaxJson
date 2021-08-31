a03efdaaf89910bb85f284d4ff5201d5e512e4a1
Ishak Alpfidan <isaac.alpines@hotmail.com>
2021-08-31 12:30:26 +0300

    Get and set data with ajax json

---
+++ AjaxJson/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxJson.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<string> listData = new List<string>() { "apple", "samsung", "xiaomi", "huawei", "asus" };
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetData()
+        {
+            return Json(listData, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult SetData(string listItem)
+        {
+            if (listItem != null)
+            {
+                listData.Add(listItem);
+            }
+
+            return Json(listData, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
